11472879776b0336fad86cf5a2c62edec19618dc
naimuru <nbello@unal.edu.co>
2024-04-02 07:41:51 -0500

    Modified AuthTypes so it handles JWTF

--- ms_types/AuthTypes.py
+++ ms_types/AuthTypes.py
@@ -1,11 +1,19 @@
 import strawberry
 from ms_types.UserSocialTypes import User, UserInput, UserDeleteInput, UserUpdateInput, FollowInput, FollowResponse
 
+@strawberry.type
+class UserAuth:
+    id: strawberry.ID
+    email: str
+    created_at: str
+    updated_at: str
+    nickname: str
+    keyIdAuth: str
 
 @strawberry.type
 class AuthToken:
     token: str
-    user: User  
+    user: UserAuth  
 
 @strawberry.type
 class AuthError:
