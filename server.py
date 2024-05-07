07743a6f25bd2bdb61575fdabb2dfcc833a52bc5
Juan Zambrano <juzambranol@unal.edu.co>
2024-05-07 01:24:56 -0500

    mll

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 # Split the URL into base and endpoint
 base_url = "host.docker.internal"
-base_external_url="localhost"
 base_endpoint = "api/v1" 
 
 # Now you can use base_url, endpoint, and port in your code
@@ -18,7 +17,7 @@ music_port="8080"
 COMMENTS_URL_BASE = "http://{0}:{1}/{2}/".format(base_url, comments_port, base_endpoint)
 USERSOCIAL_URL_BASE = "http://{0}:{1}/{2}/".format(base_url, userSocial_port, base_endpoint)
 RATING_URL_BASE = "http://{0}:{1}/{2}/".format(base_url, rating_port, base_endpoint)
-AUTH_URL_BASE = "http://{0}:{1}/{2}/".format(base_external_url, auth_port, base_endpoint) 
+AUTH_URL_BASE = "http://{0}:{1}/{2}/".format(base_url, auth_port, base_endpoint) 
 MUSIC_URL_BASE="http://{0}:{1}/{2}/".format(base_url,music_port,base_endpoint)
 
 #types
